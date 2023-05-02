04875d1bdcf16ffcb203e7b473deedc715623978
Ryoshix <108194070+Ryoshix@users.noreply.github.com>
2023-05-02 15:06:37 +0300

    Add files via upload

---
+++ Input/InputAxis.cs
@@ -0,0 +1,18 @@
+﻿public sealed class InputAxis
+{
+    #region Keyboard
+    public static string Horizontal { get => "Horizontal"; }
+    
+    public static string Vertical { get => "Vertical"; }
+    
+    public static string HorizontalQE { get => "Horizontal QE"; }
+    #endregion
+    
+    #region Mouse
+    public static string MouseX { get => "Mouse X"; }
+    
+    public static string MouseY { get => "Mouse Y"; }
+    
+    public static string MouseScrollWheel { get => "Mouse ScrollWheel"; }
+    #endregion    
+}
